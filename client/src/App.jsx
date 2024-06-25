be4caebe9abf83222cc265905d53f339eb3d9c4e
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-06-25 08:11:20 -0700

    complete navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import("./App.css")
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+
+    <>
+      <BrowserRouter>
+      <Navbar/>
+         <Routes>
+           <Route path='/' element = {<Home/>}/>
+         </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from "axios"
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+  const handleCloseSlidebar = () => {
+    setShowNavbar(false)
+  }
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  const [userData, setUserData] = useState({});
+  axios.defaults.withCredentials = true;
+  const handleLogout = () => {
+    // console.log("logout button is clicked");
+    // window.localStorage.clear();
+    // axios
+    //   .get("http://localhost:5000/auth/logout")
+    //   .then((result) => {
+    //     console.log(result.data.message);
+    //     window.location.reload();
+    //     console.log("token is clear " + result);
+    //   })
+    //   .catch((err) => console.log(err));
+  };
+
+//   useEffect(() => {
+//     axios.get('http://localhost:5000/auth/user')  // Use the correct URL
+//       .then(response => setUserData(response.data));
+//   }, []);
+
+  return (
+    <div className="navbar-container">
+      <div className="logo-side">
+        {/* <img src="/assets/logi1.png" className="logo-img" /> */}
+        <h2>MakeFriends</h2>
+
+      </div>
+      <div className="menu-icon" onClick={handleShowNavbar}>
+        <GiHamburgerMenu className='hamburger' />
+       
+      </div>
+      <div className={`nav-elements  ${showNavbar && 'active'}`}>
+      <RxCross2 className='close-nav ' onClick={handleCloseSlidebar}/>
+        <Link to="/" className="nav-link" onClick={handleCloseSlidebar}>Users</Link>
+
+        <Link to="/friends" className="nav-link" onClick={handleCloseSlidebar}>
+          Friends
+        </Link>
+
+
+        
+        {/* <Link className="nav-link none">
+          {userData.email}
+        </Link> */}
+        {window.localStorage.length > 0 ? (
+          <Link to="/" onClick={handleCloseSlidebar}>
+            <button className="login-nav-btn common-btn" onClick={handleLogout}>
+              Logout
+            </button>
+          </Link>
+        ) : (
+          <Link to="/login" onClick={handleCloseSlidebar}>
+            <button className="login-nav-btn common-btn">Login</button>
+          </Link>
+        )}
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
